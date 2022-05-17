5f6a8917543ec686b2b376aa854b5868dd7bdee0
Andresllc <105199858+Andresllc@users.noreply.github.com>
2022-05-17 06:38:41 +0200

    Add files via upload

---
+++ Reto1.js
@@ -0,0 +1,7 @@
+let suma = (n1,n2 =0) => {return n1 + n2}
+let resta= (n1,n2 =0) => {return n1 - n2}
+let divide= (n1,n2 =0) => {return n1 / n2}
+let multiplica= (n1,n2 =0) => {return n1 * n2}
+let cuadrado = (n1) => {return (Math.pow(n1,2))}
+
+module.exports = {suma, resta, divide , multiplica,cuadrado}
---
+++ Run.js
@@ -0,0 +1,7 @@
+let myLib = require("../Reto1");
+
+console.log(myLib.suma (34,35))
+console.log(myLib.resta (138,69))
+console.log(myLib.divide (138,2))
+console.log(myLib.multiplica (34.5,2))
+console.log(myLib.cuadrado (5))
